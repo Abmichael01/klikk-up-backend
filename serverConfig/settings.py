36b3a298d95ee0cf24ea09cd4e271ec897791433
Urkel <abmichael109@gmail.com>
2025-05-13 05:33:27 +0100

    New changes

--- serverConfig/settings.py
+++ serverConfig/settings.py
@@ -164,7 +164,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
